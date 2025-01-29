9e10093ddfd5de114916ec3cb47e703b709355e6
Refael Levi <refael@gamil.com>
2025-01-29 16:02:31 +0000

    add docker

--- server.js
+++ server.js
@@ -78,15 +78,26 @@ const io = require('socket.io')(server, {
     }
 })
 
+app.use((req, res, next) => {
+    res.setHeader("Content-Security-Policy", "default-src 'self'; img-src 'self' http://localhost:3030;");
+    next();
+});
+
 // Connected users map and active groups
 const connectedUsers = new Map()
 const activeGroups = new Map()
 
+app.use(express.static('public'))
 app.use(express.json())
 app.use(cors({
     origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
     credentials: true
 }))
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!')
+})
+
 
 io.on('connection', socket => {
     console.log('New client connected:', socket.id)
